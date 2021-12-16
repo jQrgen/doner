6a0bfa9552018e8c46edbe85e4e721bc5d877872
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2021-12-16 08:00:39 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,9 @@
   <hr>
 
   <br>
+  <h2>Sign up for smaapeng airdrop:</h2>
+  <iframe src="https://cdn.forms-content.sg-form.com/50d18a52-5e3d-11ec-8080-7a7dcdce489a"/>
+   <br>
   <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1322369266&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/jqrgen" title="jQrgen" target="_blank" style="color: #cccccc; text-decoration: none;">jQrgen</a> · <a href="https://soundcloud.com/jqrgen/sets/doener-tallrik-dj-bord" title="Döner Tallrik &amp; Dj Bord" target="_blank" style="color: #cccccc; text-decoration: none;">Döner Tallrik &amp; Dj Bord</a></div>
   <br>
   <br>
